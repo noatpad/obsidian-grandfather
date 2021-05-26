3f03e1248c927ea0b48dacaf950b2e5f21cc4280
Danny Hernandez <tamdannyhdz@gmail.com>
2021-05-25 19:24:26 -0500

    Add settings and time formats

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,42 @@
 import { Plugin } from 'obsidian';
 import { DateTime } from 'luxon';
+import { ISettings, SettingsTab, DEFAULT_SETTINGS } from './settings';
 
-class Grandfather extends Plugin {
+export default class Grandfather extends Plugin {
+  settings: ISettings
   statusBar: HTMLElement
 
   async onload() {
-    console.log('Loading plugin...');
+    console.log('Loading Grandfather...');
+
+    this.settings = await this.loadData() || DEFAULT_SETTINGS;
+    this.addSettingTab(new SettingsTab(this.app, this));
 
     this.statusBar = this.addStatusBarItem();
-    this.display();
+    this.updateClock();
 
-    this.registerInterval(window.setInterval(() => this.display(), 500));
+    this.registerInterval(window.setInterval(() => this.updateClock(), 500));
   }
 
-  async onunload() {
-    console.log('Unloading plugin...');
+  onunload() {
+    console.log('Unloading Grandfather...');
   }
 
-  display() {
-    const now = DateTime.now().toLocaleString(DateTime.TIME_WITH_SECONDS);
-    this.statusBar.setText(now);
+  formatTimestamp() {
+    const now = DateTime.now();
+    switch (this.settings.format) {
+      case '12h':     return now.toLocaleString(DateTime.TIME_SIMPLE);
+      case '12hs':    return now.toLocaleString(DateTime.TIME_WITH_SECONDS);
+      case '24h':     return now.toLocaleString(DateTime.TIME_24_SIMPLE);
+      case '24hs':    return now.toLocaleString(DateTime.TIME_24_WITH_SECONDS);
+      case 'dt':      return now.toLocaleString(DateTime.DATETIME_MED);
+      case 'dts':     return now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+      case 'custom':  return now.toFormat(this.settings.customFormat);
+      default:        return "Invalid time format??";
+    }
+  }
+
+  updateClock() {
+    this.statusBar.setText(this.formatTimestamp());
   }
 }
-
-export default Grandfather;
---
+++ src/settings.ts
@@ -0,0 +1,82 @@
+import { App, PluginSettingTab, Setting } from "obsidian";
+import Grandfather from './main'
+
+type Format = '12h' | '12hs' | '24h' | '24hs' | 'dt' | 'dts' | 'custom';
+
+export interface ISettings {
+  format: Format,
+  customFormat: string
+};
+
+export const DEFAULT_SETTINGS: ISettings = {
+  format: '12h',
+  customFormat: ''
+};
+
+const FORMAT_OPTIONS = {
+  '12h': '12-hour',
+  '12hs': '12-hour with seconds',
+  '24h': '24-hour',
+  '24hs': '24-hour with seconds',
+  'dt': 'Date and time',
+  'dts': 'Date and time with seconds',
+  'custom': 'Custom format'
+};
+
+const CUSTOM_FORMAT_DESCRIPTION = new DocumentFragment();
+CUSTOM_FORMAT_DESCRIPTION.append(
+  createSpan({ text: 'Type in a custom format to use. Look ' }),
+  createEl('a', { text: 'here', href: 'https://moment.github.io/luxon/docs/manual/formatting#table-of-tokens' }),
+  createSpan({ text: ' for more information' })
+);
+
+export class SettingsTab extends PluginSettingTab {
+  plugin: Grandfather;
+
+  constructor(app: App, plugin: Grandfather) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  async saveSettings(refresh: Boolean = false) {
+    await this.plugin.saveData(this.plugin.settings);
+    this.plugin.updateClock();
+    if (refresh) {
+      this.display();
+    }
+  }
+
+  display() {
+    const { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName('Format')
+      .setDesc('Set a time format to display')
+      .addDropdown(dropdown => {
+        dropdown
+          .addOptions(FORMAT_OPTIONS)
+          .setValue(this.plugin.settings.format)
+          .onChange(async (val: Format) => {
+            this.plugin.settings.format = val;
+            await this.saveSettings(true);
+          });
+      });
+
+    if (this.plugin.settings.format === 'custom') {
+      new Setting(containerEl)
+        .setName('Custom Format')
+        .setDesc(CUSTOM_FORMAT_DESCRIPTION)
+        .addText(text => {
+          text
+            .setPlaceholder('hh:mm a')
+            .setValue(this.plugin.settings.customFormat)
+            .onChange(async (val: string) => {
+              this.plugin.settings.customFormat = val;
+              await this.saveSettings();
+            });
+        });
+    }
+  }
+}
